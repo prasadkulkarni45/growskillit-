28a9c9eea59e071211702ef4c03f56b743d01900
prasad kulkarni <prasadkulkarni45.com>
2026-08-14 12:16:52 +0530

    Added succesfully

---
+++ FirstNonRepepeatCharacter.java
@@ -0,0 +1,25 @@
+package Assignments;
+
+public class FirstNonRepepeatCharacter {
+
+	public static void main(String[] args) {
+		String str = "swiss";
+		for (int i= 0; i< str.length(); i++) {
+			char ch = str.charAt(i)
+;
+			int count =0;
+			for(int j=0; j<str.length();j++) {
+				if (ch == str.charAt(j)) {
+					count++;
+				}
+				
+			}
+			if (count == 1) {
+				System.out.println("First non-repeating character:" +ch);
+				break;
+			}
+
+	}
+
+	}
+}
